6f7b62bf1e1b48877c613ceed7a340cfa131edae
medard <medardkik01@gmail.com>
2025-12-09 12:41:57 +0200

    flow condition

--- SampleFolder/scope_lab.js
+++ SampleFolder/scope_lab.js
@@ -1,39 +1,12 @@
-// Global scope
-var globalVar = "I'm a global variable";
-let globalLet = "I'm also global, but scoped with let";
-const globalConst = "I'm a global constant";
+let userRole = "admin";
+let accessLevel;
 
-
-{
-// Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
 }
 
-/*
-// Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
-
-
-//Block Scope
-console.log(blockVar);
-console.log(blockLet);
-*/
-// -=================================================================================
-
-function show(){
-var functionVar = "I'm a block-scoped var";
-let functionLet = "I'm a block-scoped let";
-const functionConst = "I'm a block-scoped const";
-}
-
-show();
-
-
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
-
+console.log("Access Level:", accessLevel);
