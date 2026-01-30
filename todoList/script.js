2d04384216ef7ddeff9f0bbcc1be421ffb4f65e7
medard <medardkik01@gmail.com>
2026-01-30 09:54:09 +0200

    TodoList IBM

--- todoList/script.js
+++ todoList/script.js
@@ -2,8 +2,5 @@ document.querySelector('#push').onclick = function(){
     if (document.querySelector('#newtask input').Value.length == 0){
         alert("please Enter a task !")
     }
-    else{
-       
-    }
 
 }
