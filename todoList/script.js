8202418f0d6b39d3ba7f8e9fcf063bd69b8acb5c
medard <medardkik01@gmail.com>
2026-01-30 09:13:54 +0200

    fix: correct script path and input field references in HTML and JS

--- todoList/script.js
+++ todoList/script.js
@@ -1,20 +1,9 @@
-document.querySelector('#push').onclick = function
-(){
-    if (document.querySelector('#newtask input').
-    ariaValueMax.length == 0){
+document.querySelector('#push').onclick = function(){
+    if (document.querySelector('#newtask input').Value.length == 0){
         alert("please Enter a task !")
     }
     else{
-        Document.querySelector('#tasks').innerHTML
-        += `
-            <div class="task">
-                <spam id="taskname">
-                    ${document.querySelector('#newTask input').value}
-                </span>
-                <button class="delete">
-                </button
-            </div>
-        `;
+       
     }
 
 }
