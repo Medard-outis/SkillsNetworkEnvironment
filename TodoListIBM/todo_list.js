9a823f342767883e2637b1cdeaa130f22dceafee
medard <medardkik01@gmail.com>
2026-01-30 10:24:14 +0200

    feat: refactor displayTasks function to improve task rendering

--- TodoListIBM/todo_list.js
+++ TodoListIBM/todo_list.js
@@ -14,3 +14,15 @@ function addTask() {
         displayTasks();
     }
 }
+
+
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+        <label for="task-${index}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+    taskList.appendChild(li);
+    });
+}
