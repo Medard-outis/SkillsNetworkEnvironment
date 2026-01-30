bcce54cee76b6ad83ae02c7aef9c2e9372d2cf76
medard <medardkik01@gmail.com>
2026-01-30 13:10:56 +0200

    feat: update HTML structure and enhance task rendering in JavaScript

--- todoList/script.js
+++ todoList/script.js
@@ -1,6 +1,21 @@
 document.querySelector('#push').onclick = function(){
+    //
     if (document.querySelector('#newtask input').value.length == 0){
         alert("please Enter a task !")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += 
+        `
+        <div class="task">
+            <span id="taskname">
+                ${document.querySelector('#newtask input').value}
+            </span>
+            
+            <button class="delete">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>
+        `
     }
 
 }
