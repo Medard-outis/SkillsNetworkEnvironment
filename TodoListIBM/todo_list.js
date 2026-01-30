e44e28f0dff902208a7cdfa44de71ed42b38e909
medard <medardkik01@gmail.com>
2026-01-30 10:19:21 +0200

    feat: implement addTask function to manage task input and display

--- TodoListIBM/todo_list.js
+++ TodoListIBM/todo_list.js
@@ -2,3 +2,15 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+
+let tasks = [];
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
