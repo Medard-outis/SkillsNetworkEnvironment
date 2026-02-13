409e4f0694fe93cc0acf3c6a6a7d477a71d7e10a
medard <medardkik01@gmail.com>
2026-02-13 06:51:12 -0500

    response type specification

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -13,3 +13,6 @@ True: Indicates if the request is asynchronous (true) or synchronous (false).
  In this case, it's set to true for asynchronous operation, allowing other 
  scripts to run while the request is processed.
  */
+
+ // response type specification
+ xhr.responseType ='json';
